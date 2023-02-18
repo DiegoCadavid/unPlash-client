cb17b3bc71a269d35ed61ed98aa160c6f5d62890
DiegoCadavid <dcmgames50@gmail.com>
2023-02-17 21:44:14 -0500

    renaming footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Created by Natalia Cadavid </p>
+      <p>Created by Diego Cadavid </p>
     </div>
   );
 };
